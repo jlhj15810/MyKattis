ad37b1c13caadde0305fafe1eaea76a0e221f3b8
Joshua <x3tvaddict@hotmail.com>
2019-07-07 13:38:15 +0800

    Testing

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+class Main{
+
+
+   public static void main(String[] args){
+      Integer y = new Integer(20);
+      Integer w;
+      w = new Integer(20);
+
+      if(w==y){
+         System.out.println("1. w == y");
+      }
+      
+      w=y;
+      if (w==y) System.out.println("2. w==y");
+
+
+      }
+}
+
