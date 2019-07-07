efb10802ccc40ba359a7a36db6716f17f34538e9
Joshua <x3tvaddict@hotmail.com>
2019-07-07 17:00:34 +0800

    First commit

---
+++ Filip/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.*;
+
+class Main {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      List<Integer> integer_list = new ArrayList<>();
+      while(sc.hasNextLine()) {
+         int first_num = sc.nextInt();
+         integer_list.add(first_num);
+   }
+      
+      for(Integer i : integer_list){
+         i.
+
+}
